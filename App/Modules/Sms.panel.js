36d108c727c371b04ac107d4b4f9e83d26fecd5e
Saeid Rahimi Golafshani <117471751+saeed-rahimi-golafshani@users.noreply.github.com>
2023-06-01 23:47:26 +0330

    Update Sms.panel.js

--- App/Modules/Sms.panel.js
+++ App/Modules/Sms.panel.js
@@ -1,9 +1,9 @@
 const trezSmsClient = require("trez-sms-client");
 
 async function smsClient(mobile, otpCode){
-    const sender = "5000248725";
+    const sender = "50000000";
     const message = `رمز یکبار مصرف ${otpCode}`
-    const client = new trezSmsClient("215Saeed", "2150057239");
+    const client = new trezSmsClient("username", "password");
     client.sendMessage(sender, mobile, message, otpCode)
     .then((receipt) =>{
         console.log("Receipt: " + receipt);
